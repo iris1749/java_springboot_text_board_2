edd01810b74500ffb8dde4f3300a6f19d2d8cdaf
iris1749 <jychoi1589@gmail.com>
2024-04-05 17:43:34 +0900

    2-04 chapter add

---
+++ src/main/java/com/korea/sbb1/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.korea.sbb1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository <Answer, Integer> {
+
+}
---
+++ src/main/java/com/korea/sbb1/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.korea.sbb1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
