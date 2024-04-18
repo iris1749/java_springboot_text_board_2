a8a13acfe5f489c269c8c6ed3eee50645f272861
iris1749 <jychoi1589@gmail.com>
2024-04-18 17:26:14 +0900

    ì±answer paging bug fix(paging question id mapping script error)

--- src/main/java/com/korea/sbb1/user/USerController.java
+++ src/main/java/com/korea/sbb1/user/USerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
